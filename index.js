74da98cdd4ce003369674043494835764cb7ce0c
matanmaia836 <natan.maia@lojaodobras.com.br>
2022-04-07 18:35:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+var texto1 = document.getElementById("b1");
+var email = document.getElementById("b2");
+
+// metado para mostrar um alerta de treinamento
+function vai() {
+    alert("Formulario enviado com sucesso!" + texto1.value + "" + email.value);
+
+}
