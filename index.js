6e2d45579c335b4febf2beb19fdae284b6fa4a22
marinaldof <marinaldofsilva@gmail.com>
2022-04-08 16:10:22 -0300

    Segundo comite

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 
-
-
 var texto1 = document.getElementById("b1");
 var email = document.getElementById("b2");
+var limp = 
 
 // metado para mostrar um alerta de treinamento
 function vai() {
     alert("Formulario enviado com sucesso!" + texto1.value + "" + email.value);
 
+}
+
+function limpar() {
+    
 }
